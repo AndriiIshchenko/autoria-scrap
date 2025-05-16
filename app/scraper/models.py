650e120b478e94017f4aaec38423e2f3321ad7b6
AndriyIshchenko <iscenkoa@gmail.com>
2025-05-16 19:58:34 +0300

    feat: add model connect mysql

---
+++ app/scraper/models.py
@@ -0,0 +1,24 @@
+from datetime import date
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Date, Numeric
+
+
+Base = declarative_base()
+
+
+class Advertisement(Base):
+    __tablename__ = "advertisements"
+
+    id = Column(Integer, primary_key=True, index=True)
+    url = Column(String, unique=True, index=True)
+    title = Column(String)
+    price_usd = Column(Numeric(10, 2))
+    odometer = Column(Integer)
+    username = Column(String)
+    phone_number = Column(Integer)
+    image_url = Column(String)
+    images_count = Column(Integer)
+    car_number = Column(String)
+    car_vin = Column(String)
+    datetime_found = Column(Date, default=date.today)
